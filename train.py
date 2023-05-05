c4487557d99f6942a817694d9b9f9dabb49146e4
ArtA <pthung7102002@gmail.com>
2023-05-06 04:00:34 +0700

    97% on VGG13

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import os
-from model import TinyVGG
+from model import VGG13
 from data_setup import dataset_generator, create_dataloaders
 from utils import train_step, test_step, save_model
 
@@ -8,58 +8,76 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import transforms as T
 
+
+# Set seed
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 
+# Variable
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-DGC = DATASET_GENERATE_CONFIG = {
-    "src_path": 'PetImages',
-    "dest_path": 'Dataset',
-    "val_size": 0.2,
-    "test_size": 0,
-    "replace": False,
-    "random_seed": 42
-}
-CLASSES = sorted(os.listdir(DGC["src_path"]))
 NUM_WORKER = os.cpu_count()
-SIZE = (128, 128)
-BATCH_SIZE = 50
-EPOCHS = 0
-LEARNING_RATE = 0.001
 
-transforms = T.Compose([
+SRC_PATH = 'PetImages'
+DEST_PATH = 'Dataset'
+TRAIN_TEST_SIZE = [0.8, 0.2]
+CREATE_DATASET = False  # Overwrite if already exist
+
+CLASSES = sorted(os.listdir(SRC_PATH))
+
+# Hyperparameter
+SIZE = (224, 224)
+LEARNING_RATE = 0.00001
+BATCH_SIZE = 16
+EPOCHS = 20
+
+# Data transforms
+train_tf = T.Compose([
     T.Resize(SIZE),
     T.RandomHorizontalFlip(),
     T.RandomRotation(10),
-    T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
-    T.ToTensor()
-])    
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]),
+])
+test_tf = T.Compose([
+    T.Resize(SIZE),
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225])
+])
 
 
-if not os.path.exists(DGC["dest_path"]) or DGC["replace"]:
-    dataset_generator(DATASET_GENERATE_CONFIG)
+# Generate the Dataset
+if not os.path.exists(DEST_PATH) or CREATE_DATASET:
+    dataset_generator(src_path=SRC_PATH,
+                      dest_path=DEST_PATH,
+                      split_size=TRAIN_TEST_SIZE,
+                      image_format=['jpg'])
 
-train_dataloader, val_dataloader, test_dataloader = create_dataloaders(data_path=DGC['dest_path'], 
-                                                                       batch_size=BATCH_SIZE, 
-                                                                       train_transform=transforms,
-                                                                       num_worker=NUM_WORKER)
+# Create the DataLoader
+train_loader, test_loader = create_dataloaders(data_path=DEST_PATH,
+                                               batch_size=BATCH_SIZE,
+                                               train_transform=train_tf,
+                                               test_transform=test_tf,
+                                               num_worker=NUM_WORKER)
 
-model = TinyVGG().to(DEVICE)
+# Model infor
+model = VGG13().to(DEVICE)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-scheduler = optim.lr_scheduler.StepLR(optimizer, 5, 0.5)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2)
+MODEL_SAVE_STEP = 2
 
+# Training loop
 for epoch in range(EPOCHS):
-    train_loss, train_acc = train_step(model, train_dataloader, criterion, optimizer, DEVICE)
+    train_loss, train_acc = train_step(model, train_loader, criterion, optimizer, DEVICE)
 
-    val_loss, val_acc = test_step(model, val_dataloader, criterion, DEVICE)
+    test_loss, test_acc = test_step(model, test_loader, criterion, DEVICE)
 
-    scheduler.step()
+    scheduler.step(test_loss)
 
-    print(f"Epoch: {epoch+1} | Lr: {optimizer.param_groups[0]['lr']}")
-    print(f"  |- loss: {train_loss:.4f} acc: {train_acc:.4f} | val_loss: {val_loss:.4f} val_acc: {val_acc:.4f}")
+    print(f"Epoch: {epoch+1} | Lr: {optimizer.param_groups[0]['lr']}                              ")
+    print(f"  |- loss: {train_loss:.4f}  acc: {train_acc:.4f} | test_loss: {test_loss:.4f}  test_acc: {test_acc:.4f}")
 
-    if (epoch+1) % 5 == 0:
-        test_loss, test_acc = test_step(model, test_dataloader, criterion, DEVICE)
-        save_model(model=model, target_dir="saved", model_name=f"{epoch+1}_epoch_{test_loss:.4f}_{test_acc:.2f}.pth")
+    if (epoch+1) % MODEL_SAVE_STEP == 0:
+        save_model(model=model, target_dir="Models", model_name=f"E{epoch+1}_L{test_loss:.4f}_A{test_acc:.2f}.pth")
