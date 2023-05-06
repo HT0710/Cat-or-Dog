2e1da9a3da184fe7821e8ca35699af48864e7e65
ArtA <pthung7102002@gmail.com>
2023-05-07 00:07:51 +0700

    add fullscreen

--- cam_test.py
+++ cam_test.py
@@ -15,6 +15,12 @@ TRANSFORM = T.Compose([
                 std=[0.229, 0.224, 0.225])
 ])
 
+
+# Camera setting
+VIDEO_SIZE = (1920, 1080)
+FULLSCREEN = True
+
+
 # Define model
 model = VGG13().to(DEVICE).eval()
 model.load_state_dict(torch.load('Models/E12_L0.0873_A0.97.pth'))
@@ -33,11 +39,15 @@ while(True):
         _, pred = torch.max(outputs, 1)
 
     # Video size
-    frame = cv2.resize(frame, (960, 540))
+    frame = cv2.resize(frame, VIDEO_SIZE)
 
     # FPS and label
-    cv2.putText(frame, str(vid.get(cv2.CAP_PROP_FPS)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.putText(frame, CLASSES[pred], (50, 500), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 3)
+    cv2.putText(frame, str(vid.get(cv2.CAP_PROP_FPS)), (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+    cv2.putText(frame, CLASSES[pred], (20, 300), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 5)
+
+    if FULLSCREEN:
+        cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     cv2.imshow('frame', frame)
 
